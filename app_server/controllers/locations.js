970e4104983e1753f7f1b7b08780724d488545de
Vince Bloise <vbloise3@verizon.net>
2016-03-29 21:52:50 -0400

    Added jade template for locations-add and about with associated controller logic

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,10 +8,10 @@ module.exports.homelist = function(req, res){
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res){
-    res.render('index', { title: 'Location info' });
+    res.render('location-info', { title: 'Location info' });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
-    res.render('index', { title: 'Add review' });
+    res.render('location-review-form', { title: 'Add review' });
 };
