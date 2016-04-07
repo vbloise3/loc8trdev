bef7abc230842a72cdbdacbacccf8ac7ba7ed09c
Vince Bloise <vbloise3@verizon.net>
2016-04-06 21:54:01 -0400

    Added basic REST structure

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -15,9 +15,34 @@ module.exports.locationsCreate = function (req, res) {
 module.exports.locationsListByDistance = function (req, res) {
     sendJSONresponse(res, 200, {"status" : "success"});
 };
-module.exports.locationsReadOne = function (req, res) {
-    sendJSONresponse(res, 200, {"status" : "success"});
+/* GET a location by the id */
+module.exports.locationsReadOne = function(req, res) {
+    console.log('Finding location details', req.params);
+    if (req.params && req.params.locationid) {
+        Loc
+            .findById(req.params.locationid)
+            .exec(function(err, location) {
+                if (!location) {
+                    sendJSONresponse(res, 404, {
+                        "message": "locationid not found"
+                    });
+                    return;
+                } else if (err) {
+                    console.log(err);
+                    sendJSONresponse(res, 404, err);
+                    return;
+                }
+                console.log(location);
+                sendJSONresponse(res, 200, location);
+            });
+    } else {
+        console.log('No locationid specified');
+        sendJSONresponse(res, 404, {
+            "message": "No locationid in request"
+        });
+    }
 };
+
 module.exports.locationsUpdateOne = function (rewq, res) {
     sendJSONresponse(res, 200, {"status" : "success"});
 };
