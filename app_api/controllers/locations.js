af023a730e41f44d8b4e462e1ed2c6fb12e968b9
Vince Bloise <vbloise3@verizon.net>
2016-04-07 20:23:38 -0400

    Added REST service to find locations by longitude and laditude

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -9,12 +9,77 @@ var sendJSONresponse = function(res, status, content) {
     res.json(content);
 };
 
+var theEarth = (function() {
+    var earthRadius = 6371; // km, miles is 3959
+
+    var getDistanceFromRads = function(rads) {
+        return parseFloat(rads * earthRadius);
+    };
+
+    var getRadsFromDistance = function(distance) {
+        return parseFloat(distance / earthRadius);
+    };
+
+    return {
+        getDistanceFromRads: getDistanceFromRads,
+        getRadsFromDistance: getRadsFromDistance
+    };
+})();
+
 module.exports.locationsCreate = function (req, res) {
     sendJSONresponse(res, 200, {"status" : "success"});
 };
-module.exports.locationsListByDistance = function (req, res) {
-    sendJSONresponse(res, 200, {"status" : "success"});
+
+/* GET list of locations */
+module.exports.locationsListByDistance = function(req, res) {
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var maxDistance = parseFloat(req.query.maxDistance);
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+    var geoOptions = {
+        spherical: true,
+        maxDistance: theEarth.getRadsFromDistance(maxDistance),
+        num: 10
+    };
+    if (!lng || !lat || !maxDistance) {
+        console.log('locationsListByDistance missing params');
+        sendJSONresponse(res, 404, {
+            "message": "lng, lat and maxDistance query parameters are all required"
+        });
+        return;
+    }
+    Loc.geoNear(point, geoOptions, function(err, results, stats) {
+        var locations;
+        console.log('Geo Results', results);
+        console.log('Geo stats', stats);
+        if (err) {
+            console.log('geoNear error:', err);
+            sendJSONresponse(res, 404, err);
+        } else {
+            locations = buildLocationList(req, res, results, stats);
+            sendJSONresponse(res, 200, locations);
+        }
+    });
 };
+
+var buildLocationList = function(req, res, results, stats) {
+    var locations = [];
+    results.forEach(function(doc) {
+        locations.push({
+            distance: theEarth.getDistanceFromRads(doc.dis),
+            name: doc.obj.name,
+            address: doc.obj.address,
+            rating: doc.obj.rating,
+            facilities: doc.obj.facilities,
+            _id: doc.obj._id
+        });
+    });
+    return locations;
+};
+
 /* GET a location by the id */
 module.exports.locationsReadOne = function(req, res) {
     console.log('Finding location details', req.params);
