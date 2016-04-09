f1225c29e6eea1cd22857c03a37a8bf1f05164c2
Vince Bloise <vbloise3@verizon.net>
2016-04-09 10:40:44 -0400

    Added REST service to create locations

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -26,8 +26,35 @@ var theEarth = (function() {
     };
 })();
 
-module.exports.locationsCreate = function (req, res) {
-    sendJSONresponse(res, 200, {"status" : "success"});
+/* POST a new location */
+/* /api/locations */
+module.exports.locationsCreate = function(req, res) {
+    console.log(req.body);
+    Loc.create({
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+        openingTimes: [{
+            days: req.body.days1,
+            opening: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1,
+        }, {
+            days: req.body.days2,
+            opening: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2,
+        }]
+    }, function(err, location) {
+        if (err) {
+            console.log(err);
+            sendJSONresponse(res, 400, err);
+        } else {
+            console.log(location);
+            sendJSONresponse(res, 201, location);
+        }
+    });
 };
 
 /* GET list of locations */
