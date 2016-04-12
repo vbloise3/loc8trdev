a0a7ba18c3fa473d0560d051b41d47981076f69d
Vince Bloise <vbloise3@verizon.net>
2016-04-11 21:05:38 -0400

    Fixed distance in first display of REST response

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -26,37 +26,6 @@ var theEarth = (function() {
     };
 })();
 
-/* POST a new location */
-/* /api/locations */
-module.exports.locationsCreate = function(req, res) {
-    console.log(req.body);
-    Loc.create({
-        name: req.body.name,
-        address: req.body.address,
-        facilities: req.body.facilities.split(","),
-        coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
-        openingTimes: [{
-            days: req.body.days1,
-            opening: req.body.opening1,
-            closing: req.body.closing1,
-            closed: req.body.closed1,
-        }, {
-            days: req.body.days2,
-            opening: req.body.opening2,
-            closing: req.body.closing2,
-            closed: req.body.closed2,
-        }]
-    }, function(err, location) {
-        if (err) {
-            console.log(err);
-            sendJSONresponse(res, 400, err);
-        } else {
-            console.log(location);
-            sendJSONresponse(res, 201, location);
-        }
-    });
-};
-
 /* GET list of locations */
 module.exports.locationsListByDistance = function(req, res) {
     var lng = parseFloat(req.query.lng);
@@ -71,7 +40,7 @@ module.exports.locationsListByDistance = function(req, res) {
         maxDistance: theEarth.getRadsFromDistance(maxDistance),
         num: 10
     };
-    if (!lng || !lat || !maxDistance) {
+    if ((!lng && lng!==0) || (!lat && lat!==0) || ! maxDistance) {
         console.log('locationsListByDistance missing params');
         sendJSONresponse(res, 404, {
             "message": "lng, lat and maxDistance query parameters are all required"
@@ -135,6 +104,37 @@ module.exports.locationsReadOne = function(req, res) {
     }
 };
 
+/* POST a new location */
+/* /api/locations */
+module.exports.locationsCreate = function(req, res) {
+    console.log(req.body);
+    Loc.create({
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+        openingTimes: [{
+            days: req.body.days1,
+            opening: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1
+        }, {
+            days: req.body.days2,
+            opening: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2
+        }]
+    }, function(err, location) {
+        if (err) {
+            console.log(err);
+            sendJSONresponse(res, 400, err);
+        } else {
+            console.log(location);
+            sendJSONresponse(res, 201, location);
+        }
+    });
+};
+
 /* PUT /api/locations/:locationid */
 module.exports.locationsUpdateOne = function(req, res) {
     if (!req.params.locationid) {
@@ -165,12 +165,12 @@ module.exports.locationsUpdateOne = function(req, res) {
                     days: req.body.days1,
                     opening: req.body.opening1,
                     closing: req.body.closing1,
-                    closed: req.body.closed1,
+                    closed: req.body.closed1
                 }, {
                     days: req.body.days2,
                     opening: req.body.opening2,
                     closing: req.body.closing2,
-                    closed: req.body.closed2,
+                    closed: req.body.closed2
                 }];
                 location.save(function(err, location) {
                     if (err) {
