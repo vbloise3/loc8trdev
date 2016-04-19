e1a19834fbaf507efd7fe089aecc893b00c7e352
Vince Bloise <vbloise3@verizon.net>
2016-04-18 20:37:34 -0400

    Added added error checking to review REST api

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -124,7 +124,7 @@ module.exports.doAddReview = function(req, res){
         json : postdata
     };
     if (!postdata.author || !postdata.rating || !postdata.reviewText) {
-        res.redirect('/location/' + locationid + '/reviews/new?err=val');
+        res.redirect('/location/' + locationid + '/review/new?err=val');
     } else {
         request(
             requestOptions,
@@ -132,7 +132,7 @@ module.exports.doAddReview = function(req, res){
                 if (response.statusCode === 201) {
                     res.redirect('/location/' + locationid);
                 } else if (response.statusCode === 400 && body.name && body.name === "ValidationError" ) {
-                    res.redirect('/location/' + locationid + '/reviews/new?err=val');
+                    res.redirect('/location/' + locationid + '/review/new?err=val');
                 } else {
                     console.log(body);
                     _showError(req, res, response.statusCode);
